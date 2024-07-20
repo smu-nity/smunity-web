8038f25ff9c009267987bdf3627a4dca10742905
hyunmin0317 <choihm9903@naver.com>
2024-07-20 17:24:31 +0900

    refactor: 코드 리팩토링 (#7)

--- src/components/main/StatComponent.tsx
+++ src/components/main/StatComponent.tsx
@@ -5,17 +5,11 @@ const StatComponent = () => {
         <div>
           <i className="fas fa-calendar-day"></i>&nbsp;&nbsp;오늘 방문자 수 : 105
         </div>
-        <div
-          style={{
-            marginTop: '0.4rem'
-          }}>
+        <div style={{marginTop: '0.4rem'}}>
           <i className="fas fa-mouse" style={{marginRight: '0.1rem'}}></i>&nbsp;&nbsp;총
           방문자 수 : 73,070
         </div>
-        <div
-          style={{
-            marginTop: '0.4rem'
-          }}>
+        <div style={{marginTop: '0.4rem'}}>
           <i className="fas fa-user"></i>&nbsp;&nbsp;총 회원 수 : 3,659
         </div>
       </div>
