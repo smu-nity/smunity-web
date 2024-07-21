e8fc1250009bbb8543500a0c1814e3a945557214
hyunmin0317 <choihm9903@naver.com>
2024-07-21 14:23:52 +0900

    refactor: 코드 리팩토링 (#7)

--- src/components/main/HeadComponent.tsx
+++ src/components/main/HeadComponent.tsx
@@ -1,14 +1,14 @@
-import {useNavigate} from 'react-router-dom'
+import useCustomMove, {TCustomMove} from '../../hooks/useCustomMove'
 
 const HeadComponent = () => {
-  const navigate = useNavigate()
+  const {moveToPath}: TCustomMove = useCustomMove()
 
   return (
     <div className="main-img__text main-margin">
       <div className="bg_small_text fadein_text">상명대 졸업요건 검사 사이트</div>
       <div className="bg_big_text fadein_text">SMUNITY</div>
       <div className="bg_btn">
-        <button className="bg_btn_style" onClick={() => navigate('/mypage')}>
+        <button className="bg_btn_style" onClick={() => moveToPath('/mypage')}>
           검사하기
         </button>
       </div>
--- src/hooks/useCustomLogin.ts
+++ src/hooks/useCustomLogin.ts
@@ -1,4 +1,3 @@
-import {useNavigate} from 'react-router-dom'
 import {login, TLoginParam} from '../api/accountsApi'
 import {Member} from '../interfaces/Member'
 import {useRecoilState, useResetRecoilState} from 'recoil'
@@ -9,23 +8,21 @@ export interface TCusotmLogin {
   loginState: Member
   doLogin: (loginParam: TLoginParam) => Promise<any>
   doLogout: () => void
-  moveToPath: (path: string) => void
-  moveToLogin: () => void
   saveAsCookie: (data: Member) => void
 }
 
 const useCustomLogin = (): TCusotmLogin => {
-  const navigate = useNavigate()
-
   const [loginState, setLoginState] = useRecoilState(signinState)
   const resetState = useResetRecoilState(signinState)
 
   //----------로그인 함수
   const doLogin = async (loginParam: TLoginParam) => {
-    const result = await login(loginParam)
-    console.log(result)
-    saveAsCookie(result)
-    return result
+    const response = await login(loginParam)
+    const success = response.status < 400
+    if (success) {
+      saveAsCookie(response.data)
+    }
+    return success
   }
 
   const saveAsCookie = (data: Member) => {
@@ -39,17 +36,7 @@ const useCustomLogin = (): TCusotmLogin => {
     resetState()
   }
 
-  //----------------페이지 이동
-  const moveToPath = (path: string) => {
-    navigate({pathname: path}, {replace: true})
-  }
-
-  //----------------------로그인 페이지로 이동
-  const moveToLogin = () => {
-    navigate({pathname: '/member/login'}, {replace: true})
-  }
-
-  return {loginState, doLogin, doLogout, moveToPath, moveToLogin, saveAsCookie}
+  return {loginState, doLogin, doLogout, saveAsCookie}
 }
 
 export default useCustomLogin
