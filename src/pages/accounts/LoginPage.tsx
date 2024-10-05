caf863021b29d66acef6c60922bf8ebc3d0ee877
hyunmin0317 <hyunmin-choi@naver.com>
2024-10-05 12:15:47 +0900

    refactor: 의존성 순서 변경 (#8)

--- src/pages/accounts/LoginPage.tsx
+++ src/pages/accounts/LoginPage.tsx
@@ -1,5 +1,5 @@
-import LoginForm from '../../components/accounts/LoginForm'
 import Header from '../../components/Header'
+import LoginForm from '../../components/accounts/LoginForm'
 
 const LoginPage = () => {
   return (
