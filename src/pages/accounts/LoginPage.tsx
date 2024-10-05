3618cf075039f3c058b4be6033703d74f92b6252
hyunmin0317 <hyunmin-choi@naver.com>
2024-10-05 12:06:41 +0900

    refactor: 코드 리팩토링 (#8)

--- src/pages/accounts/LoginPage.tsx
+++ src/pages/accounts/LoginPage.tsx
@@ -5,7 +5,9 @@ const LoginPage = () => {
   return (
     <>
       <Header title="로그인" subtitle="학번과 스뮤니티 비밀번호를 입력해 주세요." />
-      <LoginForm />
+      <div className="login-container">
+        <LoginForm />
+      </div>
     </>
   )
 }
