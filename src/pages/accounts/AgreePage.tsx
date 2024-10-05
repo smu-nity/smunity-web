f7830218a99c630912a71294c6a5a35e0510abee
hyunmin0317 <hyunmin-choi@naver.com>
2024-10-05 11:58:18 +0900

    feat: 이용약관 동의 페이지 (#8)

---
+++ src/pages/accounts/AgreePage.tsx
@@ -0,0 +1,14 @@
+import Header from '../../components/Header'
+
+const AgreePage = () => {
+  return (
+    <>
+      <Header
+        title="이용약관 동의"
+        subtitle="아래 사항을 모두 읽고 약관에 동의해주세요."
+      />
+    </>
+  )
+}
+
+export default AgreePage
--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Outlet, Route, Routes} from 'react-router-dom'
 import Layout from './Layout'
 import MainPage from '../pages/MainPage'
 import LoginPage from '../pages/accounts/LoginPage'
+import AgreePage from '../pages/accounts/AgreePage'
 
 const MainRouter = (): JSX.Element => {
   return (
@@ -12,6 +13,7 @@ const MainRouter = (): JSX.Element => {
           <Route index element={<MainPage />} />
           <Route path="/accounts" element={<Outlet />}>
             <Route path="login" element={<LoginPage />} />
+            <Route path="agree" element={<AgreePage />} />
           </Route>
         </Route>
       </Routes>
