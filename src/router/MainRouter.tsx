352378d2d084b58e78ed2a00d943d2a9ea9a860c
hyunmin0317 <hyunmin-choi@naver.com>
2024-10-09 15:20:46 +0900

    feat: 라우팅 설정 (#9)

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -1,10 +1,19 @@
-import {BrowserRouter, Outlet, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Navigate, Outlet, Route, Routes} from 'react-router-dom'
 
 import Layout from './Layout'
 import MainPage from '../pages/MainPage'
 import LoginPage from '../pages/accounts/LoginPage'
 import AgreePage from '../pages/accounts/AgreePage'
 import RegisterPage from '../pages/accounts/RegisterPage'
+import MyPage from '../pages/mypage/MyPage'
+import ResultPage from '../pages/mypage/ResultPage'
+import useCustomAccount, {TCustomAccount} from '../hooks/useCustomAccount'
+import {ReactNode} from 'react'
+
+const ProtectedRoute = ({children}: {children: ReactNode}): JSX.Element => {
+  const {isLogin}: TCustomAccount = useCustomAccount()
+  return isLogin() ? <>{children}</> : <Navigate to="/accounts/login" />
+}
 
 const MainRouter = (): JSX.Element => {
   return (
@@ -16,6 +25,16 @@ const MainRouter = (): JSX.Element => {
             <Route path="login" element={<LoginPage />} />
             <Route path="agree" element={<AgreePage />} />
             <Route path="register" element={<RegisterPage />} />
+          </Route>
+          <Route
+            path="/mypage"
+            element={
+              <ProtectedRoute>
+                <Outlet />
+              </ProtectedRoute>
+            }>
+            <Route index element={<MyPage />} />
+            <Route path="result" element={<ResultPage />} />
           </Route>
         </Route>
       </Routes>
