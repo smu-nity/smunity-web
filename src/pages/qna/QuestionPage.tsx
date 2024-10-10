b82bec7d3f10fb3868d55fafb6bafaba3074d3b7
hyunmin0317 <hyunmin-choi@naver.com>
2024-10-10 21:08:24 +0900

    feat: Q&A 게시판 페이지 (#12)

--- src/pages/qna/QuestionPage.tsx
+++ src/pages/qna/QuestionPage.tsx
@@ -1,7 +1,15 @@
 import Header from '../../components/Header'
+import QuestionList from '../../components/qna/QuestionList'
 import TabComponent from '../../components/qna/TabComponent'
+import useCustomMove, {TCustomMove} from '../../hooks/useCustomMove'
 
 const QuestionPage = () => {
+  const {moveToPath}: TCustomMove = useCustomMove()
+
+  const handleClick = () => {
+    moveToPath('/qna/questions/create')
+  }
+
   return (
     <>
       <Header
@@ -11,9 +19,27 @@ const QuestionPage = () => {
       />
       <section className="cs_wrap">
         <TabComponent />
-        <div className="login-container term-container">
-          <div className="container">
-            <div className="info_box_wrapper"></div>
+        <div className="cs_area">
+          <div className="cs_body ">
+            <div className="wrap">
+              <div className="ct_petitions">
+                <div className="ct_petitions_area ct_txt_left">
+                  <div className="ct_list1">
+                    <div className="board text">
+                      <div className="b_title">
+                        <a
+                          className="btn btn-primary button-sm"
+                          style={{float: 'right', marginBottom: '5px'}}
+                          onClick={handleClick}>
+                          질문 등록
+                        </a>
+                      </div>
+                      <QuestionList />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
---
+++ src/types/Question.ts
@@ -0,0 +1,9 @@
+export interface Question {
+  id: number
+  title: string
+  content: string
+  author: string
+  answered: boolean
+  createdAt: string
+  updatedAt: string
+}
