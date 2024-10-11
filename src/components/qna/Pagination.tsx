b3e1ec24e657c4569cb9f4455fcef7a7100a53a4
hyunmin0317 <hyunmin-choi@naver.com>
2024-10-11 21:49:38 +0900

    feat: 페이지네이션 (#13)

--- src/components/qna/Pagination.tsx
+++ src/components/qna/Pagination.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import {useNavigate} from 'react-router-dom'
 import {Page} from '../../types/Page'
 import {Question} from '../../types/Question'
 
@@ -7,28 +7,48 @@ interface PaginationProps {
 }
 
 const Pagination: React.FC<PaginationProps> = ({page}) => {
-  const {totalPages} = page
+  const {totalPages, number} = page
+  const navigate = useNavigate()
+
+  const onPageChange = (pageNumber: number) => {
+    if (pageNumber >= 0 && pageNumber < totalPages) {
+      navigate(`?page=${pageNumber + 1}`)
+    }
+  }
+
+  const handlePrevious = () => {
+    if (!page.first) {
+      onPageChange(number - 1)
+    }
+  }
+
+  const handleNext = () => {
+    if (!page.last) {
+      onPageChange(number + 1)
+    }
+  }
+
   return (
     <ul className="pagination justify-content-center" style={{marginBottom: '60px'}}>
-      {page.first ? (
-        <li className="page-item disabled">
-          <a className="page-link">이전</a>
-        </li>
-      ) : (
-        <li className="page-item">
-          <a className="page-link">이전</a>
-        </li>
-      )}
+      <li className={`page-item ${page.first ? 'disabled' : ''}`}>
+        <a className="page-link" onClick={handlePrevious}>
+          이전
+        </a>
+      </li>
 
-      {page.last ? (
-        <li className="page-item disabled">
-          <a className="page-link">다음</a>
+      {Array.from({length: totalPages}, (_, idx) => (
+        <li key={idx} className={`page-item ${number === idx ? 'active' : ''}`}>
+          <a className="page-link" onClick={() => onPageChange(idx)}>
+            {idx + 1}
+          </a>
         </li>
-      ) : (
-        <li className="page-item">
-          <a className="page-link">다음</a>
-        </li>
-      )}
+      ))}
+
+      <li className={`page-item ${page.last ? 'disabled' : ''}`}>
+        <a className="page-link" onClick={handleNext}>
+          다음
+        </a>
+      </li>
     </ul>
   )
 }
