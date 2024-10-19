a74e48bcad27f374183f797d6ec801deeeebcfe8
hyunmin0317 <hyunmin-choi@naver.com>
2024-10-19 10:57:25 +0900

    refactor: 코드 리팩토링 (#13)

--- src/api/accountApi.ts
+++ src/api/accountApi.ts
@@ -14,19 +14,14 @@ export type TRegisterParam = {
   department: string
 }
 
-const postRequest = async (url: string, data: any): Promise<AxiosResponse> => {
-  const header = {headers: {'Content-Type': 'application/json'}}
-  return await api.post(url, JSON.stringify(data), header)
-}
-
 export const login = async (loginParam: TLoginParam): Promise<AxiosResponse> => {
-  return postRequest('/api/v1/accounts/login', loginParam)
+  return await api.post('/api/v1/accounts/login', loginParam)
 }
 
 export const auth = async (loginParam: TLoginParam): Promise<AxiosResponse> => {
-  return postRequest('/api/v1/auth', loginParam)
+  return await api.post('/api/v1/auth', loginParam)
 }
 
 export const register = async (registerParam: TRegisterParam): Promise<AxiosResponse> => {
-  return postRequest('/api/v1/accounts/register', registerParam)
+  return await api.post('/api/v1/accounts/register', registerParam)
 }
