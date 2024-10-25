a5b16b1e485508cac53dbfb082f4b4fa215d4b9e
hyunmin0317 <hyunmin-choi@naver.com>
2024-10-25 20:03:19 +0900

    feat: 답변 API 사용 hook 개발 (#14)

--- src/api/answerApi.ts
+++ src/api/answerApi.ts
@@ -5,10 +5,10 @@ import {AnswerRequest} from '../types/Answer'
 export const fetchAnswer = async (id: string): Promise<AxiosResponse> =>
   await jwtAxios.get(`/api/v1/questions/${id}/answer`)
 
-export const createAnswer = async (id: number, request: AnswerRequest) =>
+export const createAnswer = async (id: string, request: AnswerRequest) =>
   await jwtAxios.post(`/api/v1/questions/${id}/answer`, request)
 
-export const updateAnswer = async (id: number, request: AnswerRequest) =>
+export const updateAnswer = async (id: string, request: AnswerRequest) =>
   await jwtAxios.put(`/api/v1/questions/${id}/answer`, request)
 
 export const deleteAnswer = async (id: number) =>
--- src/hooks/useCustomAnswer.ts
+++ src/hooks/useCustomAnswer.ts
@@ -1,8 +1,10 @@
-import {fetchAnswer} from '../api/answerApi'
-import {Answer} from '../types/Answer'
+import {fetchAnswer, createAnswer, updateAnswer} from '../api/answerApi'
+import {Answer, AnswerRequest} from '../types/Answer'
 
 export interface TCustomAnswer {
   doFetchAnswer: (id: string) => Promise<Answer>
+  doCreateAnswer: (id: string, request: AnswerRequest) => Promise<boolean>
+  doUpdateAnswer: (id: string, request: AnswerRequest) => Promise<boolean>
 }
 
 const useCustomAnswer = (): TCustomAnswer => {
@@ -12,7 +14,21 @@ const useCustomAnswer = (): TCustomAnswer => {
     return success ? response.data : null
   }
 
-  return {doFetchAnswer}
+  const doCreateAnswer = async (id: string, request: AnswerRequest) => {
+    const response = await createAnswer(id, request)
+    const success = response.status < 400
+    !success && alert(response.data.message)
+    return success
+  }
+
+  const doUpdateAnswer = async (id: string, request: AnswerRequest) => {
+    const response = await updateAnswer(id, request)
+    const success = response.status < 400
+    !success && alert(response.data.message)
+    return success
+  }
+
+  return {doFetchAnswer, doCreateAnswer, doUpdateAnswer}
 }
 
 export default useCustomAnswer
