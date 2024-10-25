1ca55122751169ce9e5f2d7a9cee5e9c29cb29b0
hyunmin0317 <hyunmin-choi@naver.com>
2024-10-25 19:53:22 +0900

    feat: 답변 API 적용 (#14)

--- src/api/answerApi.ts
+++ src/api/answerApi.ts
@@ -1,10 +1,15 @@
 import {AxiosResponse} from 'axios'
 import jwtAxios from '../util/jwtUtil'
+import {AnswerRequest} from '../types/Answer'
 
-export const fetchAnswer = async (id: string): Promise<AxiosResponse> => {
-  return await jwtAxios.get(`/api/v1/questions/${id}/answer`)
-}
+export const fetchAnswer = async (id: string): Promise<AxiosResponse> =>
+  await jwtAxios.get(`/api/v1/questions/${id}/answer`)
 
-export const deleteAnswer = async (id: number) => {
+export const createAnswer = async (id: number, request: AnswerRequest) =>
+  await jwtAxios.post(`/api/v1/questions/${id}/answer`, request)
+
+export const updateAnswer = async (id: number, request: AnswerRequest) =>
+  await jwtAxios.put(`/api/v1/questions/${id}/answer`, request)
+
+export const deleteAnswer = async (id: number) =>
   await jwtAxios.delete(`/api/v1/questions/${id}/answer`)
-}
