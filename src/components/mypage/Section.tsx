edcdff232037d07454c575825b2f7cdb51dbebc5
hyunmin0317 <hyunmin-choi@naver.com>
2024-11-30 15:41:58 +0900

    feat: 마이페이지 모달 (#30)

---
+++ src/components/mypage/Section.tsx
@@ -0,0 +1,31 @@
+type SectionProps = {
+  icon: string
+  title: string
+  buttonLabel: string
+  onClick: () => void
+  children?: React.ReactNode
+}
+
+const Section: React.FC<SectionProps> = ({
+  icon,
+  title,
+  buttonLabel,
+  onClick,
+  children
+}: SectionProps) => (
+  <>
+    <div className="my_box_title">
+      <div>
+        <i className={icon}></i>
+        {title}
+      </div>
+      <button className="my_box_mod_btn" onClick={onClick}>
+        {buttonLabel}
+      </button>
+    </div>
+    {children}
+    <hr style={{margin: '0.8rem 0'}} />
+  </>
+)
+
+export default Section
