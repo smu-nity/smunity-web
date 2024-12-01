ecb1e864ed9fce6d5ba1b64221fa3d061bcfdf02
hyunmin0317 <hyunmin-choi@naver.com>
2024-12-01 16:03:37 +0900

    style: 스타일 수정 (#36)

--- src/components/main/StatComponent.tsx
+++ src/components/main/StatComponent.tsx
@@ -3,14 +3,13 @@ const StatComponent = () => {
     <div className="main-img__text card-margin">
       <div className="num_visit cards">
         <div>
-          <i className="fas fa-calendar-day"></i>&nbsp;&nbsp;오늘 방문자 수 : 105
+          <i className="fas fa-calendar-day"></i>오늘 방문자 수 : 105
         </div>
         <div style={{marginTop: '0.4rem'}}>
-          <i className="fas fa-mouse" style={{marginRight: '0.1rem'}}></i>&nbsp;&nbsp;총
-          방문자 수 : 73,070
+          <i className="fas fa-mouse"></i>총 방문자 수 : 73,070
         </div>
         <div style={{marginTop: '0.4rem'}}>
-          <i className="fas fa-user"></i>&nbsp;&nbsp;총 회원 수 : 3,659
+          <i className="fas fa-user"></i>총 회원 수 : 3,659
         </div>
       </div>
     </div>
