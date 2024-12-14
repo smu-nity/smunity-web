7d62b75ab58e4a291a31f76a14d15ba4f4bd2f0b
hyunmin0317 <hyunmin-choi@naver.com>
2024-12-14 14:19:51 +0900

    feat: 학과 선택바 추가 (#37)

---
+++ src/components/mypage/ChangeDepartment.tsx
@@ -0,0 +1,13 @@
+const ChangeDepartment = () => {
+  return (
+    <td className="regi_box" style={{width: '90%', marginLeft: '0', marginRight: '1rem'}}>
+      <select id="major">
+        <option value="7">핀테크전공</option>
+        <option value="8">빅데이터융합전공</option>
+        <option value="9">스마트생산전공</option>
+      </select>
+    </td>
+  )
+}
+
+export default ChangeDepartment
