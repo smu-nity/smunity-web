d9e0cf2d24f45af38f588238040bfffd0e05f791
hyunmin0317 <hyunmin-choi@naver.com>
2024-12-15 11:33:37 +0900

    feat: 마이페지이 로딩 스피너 추가 (#45)

--- src/components/mypage/ChangePasswordForm.tsx
+++ src/components/mypage/ChangePasswordForm.tsx
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 import useCustomMypage, {TCustomMypage} from '../../hooks/useCustomMypage'
 import useCustomAccount, {TCustomAccount} from '../../hooks/useCustomAccount'
+import LoadingSpinner from '../LoadingSpinner'
 
 interface PasswordCredentials {
   password1: string
@@ -8,12 +9,13 @@ interface PasswordCredentials {
 }
 
 const ChangePasswordFrom = () => {
-  const {passwordChange}: TCustomMypage = useCustomMypage()
-  const {doLogout}: TCustomAccount = useCustomAccount()
   const [passwordParams, setPasswordParams] = useState<PasswordCredentials>({
     password1: '',
     password2: ''
   })
+  const [isLoading, setIsLoading] = useState(false)
+  const {passwordChange}: TCustomMypage = useCustomMypage()
+  const {doLogout}: TCustomAccount = useCustomAccount()
 
   const requestParam = () => {
     const {password1: password} = passwordParams
@@ -26,16 +28,22 @@ const ChangePasswordFrom = () => {
   }
 
   const handleClick = () => {
-    passwordParams.password1 && passwordParams.password2
-      ? passwordParams.password1 === passwordParams.password2
-        ? passwordChange(requestParam()).then(success => {
-            if (success) {
-              alert('비밀번호가 변경되었습니다.')
-              doLogout()
-            }
-          })
-        : alert('비밀번호가 일치하지 않습니다.')
-      : alert('비밀번호를 입력해주세요.')
+    if (passwordParams.password1 && passwordParams.password2) {
+      if (passwordParams.password1 === passwordParams.password2) {
+        setIsLoading(true)
+        passwordChange(requestParam()).then(success => {
+          setIsLoading(false)
+          if (success) {
+            alert('비밀번호가 변경되었습니다.')
+            doLogout()
+          }
+        })
+      } else {
+        alert('비밀번호가 일치하지 않습니다.')
+      }
+    } else {
+      alert('비밀번호를 입력해주세요.')
+    }
   }
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
@@ -72,6 +80,7 @@ const ChangePasswordFrom = () => {
         value="변경하기"
         onClick={handleClick}
       />
+      {isLoading && <LoadingSpinner />}
     </>
   )
 }
