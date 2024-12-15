774a2f1c1f93b5544c2c906e8e5021eedcce8d15
hyunmin0317 <hyunmin-choi@naver.com>
2024-12-15 11:14:46 +0900

    fix: 로딩 스피너 수정 (#45)

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ const LoadingSpinner = () => {
   return (
     <div
       style={{
-        position: 'absolute',
+        position: 'fixed',
         top: '0',
         left: '0',
         width: '100%',
