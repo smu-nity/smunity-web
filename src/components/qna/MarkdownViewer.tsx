a864c3daeede736477bd43e917572a7bd7bebf0b
Hyunmin Choi <choihm9903@naver.com>
2025-01-11 09:37:32 +0900

    [FEATURE] Q&A 게시판 개행 처리 (#68)
    * chore: remark-breaks 의존성 추가 (#67)
    
    * feat: remark-breaks 플러그인 추가 (#67)

--- src/components/qna/MarkdownViewer.tsx
+++ src/components/qna/MarkdownViewer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import '../../styles/markdown.css'
+import remarkBreaks from 'remark-breaks'
 
 interface MarkdownViewerProps {
   content: string
@@ -9,7 +10,7 @@ interface MarkdownViewerProps {
 const MarkdownViewer: React.FC<MarkdownViewerProps> = ({content}) => {
   return (
     <div className="markdown-viewer">
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown remarkPlugins={[remarkBreaks]}>{content}</ReactMarkdown>
     </div>
   )
 }
