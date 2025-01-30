acacaf5e463ca7eac7dff10e0253d7a3749c9f0b
hyunmin0317 <hyunmin-choi@naver.com>
2025-01-30 17:51:55 +0900

    fix: 테이블 구조 수정 (#87)

--- src/components/mypage/InfoTable.tsx
+++ src/components/mypage/InfoTable.tsx
@@ -9,34 +9,36 @@ interface InfoTableProps {
 const InfoTable: React.FC<InfoTableProps> = ({member}) => {
   return (
     <table>
-      <tr>
-        <td className="my_box_table_1st_td">이름</td>
-        <td>{member?.name}</td>
-      </tr>
-      <tr>
-        <td className="my_box_table_1st_td">학번</td>
-        <td>{member?.username}</td>
-      </tr>
-      <tr>
-        <td className="my_box_table_1st_td">학과</td>
-        {member?.deptEditable ? (
-          <ChangeDepartment department={member?.department} />
-        ) : (
-          <td>
-            <a
-              className="link_site"
-              href={`https://www.smu.ac.kr/_custom/smu/_app/curriculum.do?srSust=${member?.deptCode}&srShyr=all`}
-              target="_blank"
-              rel="noopener noreferrer">
-              {member?.department}
-            </a>
-          </td>
-        )}
-      </tr>
-      <tr>
-        <td className="my_box_table_1st_td">이수 면제</td>
-        <ChangeExemption exemption={member?.exemption} />
-      </tr>
+      <tbody>
+        <tr>
+          <td className="my_box_table_1st_td">이름</td>
+          <td>{member?.name}</td>
+        </tr>
+        <tr>
+          <td className="my_box_table_1st_td">학번</td>
+          <td>{member?.username}</td>
+        </tr>
+        <tr>
+          <td className="my_box_table_1st_td">학과</td>
+          {member?.deptEditable ? (
+            <ChangeDepartment department={member?.department} />
+          ) : (
+            <td>
+              <a
+                className="link_site"
+                href={`https://www.smu.ac.kr/_custom/smu/_app/curriculum.do?srSust=${member?.deptCode}&srShyr=all`}
+                target="_blank"
+                rel="noopener noreferrer">
+                {member?.department}
+              </a>
+            </td>
+          )}
+        </tr>
+        <tr>
+          <td className="my_box_table_1st_td">이수 면제</td>
+          <ChangeExemption exemption={member?.exemption} />
+        </tr>
+      </tbody>
     </table>
   )
 }
