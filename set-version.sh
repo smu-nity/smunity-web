54fa4ebb20613396d2fe8eb1ee8b4e770bf8ae2a
hyunmin0317 <hyunmin-choi@naver.com>
2025-10-09 20:09:04 +0900

    refactor: 코드 리팩토링 (#120)

--- set-version.sh
+++ set-version.sh
@@ -9,15 +9,9 @@ VERSION_VALUE=$(git describe --tags --abbrev=0)
 COMMIT_VALUE=$(git rev-parse --short HEAD)
 YEAR_VALUE=$(date +%Y)
 
-# 기존 키 제거 및 나머지 보존
-grep -v "^$VERSION_KEY=" "$ENV_FILE" 2>/dev/null | \
-grep -v "^$COMMIT_KEY=" | \
-grep -v "^$YEAR_KEY=" > "$ENV_FILE.tmp" || touch "$ENV_FILE.tmp"
-
-# 새 값 추가
+touch "$ENV_FILE.tmp"
 echo "$VERSION_KEY=$VERSION_VALUE" >> "$ENV_FILE.tmp"
 echo "$COMMIT_KEY=$COMMIT_VALUE" >> "$ENV_FILE.tmp"
 echo "$YEAR_KEY=$YEAR_VALUE" >> "$ENV_FILE.tmp"
 
-# 덮어쓰기
 mv "$ENV_FILE.tmp" "$ENV_FILE"
