a850f2569061f59ee08881891915bb59951a6709
hyunmin0317 <hyunmin-choi@naver.com>
2025-10-09 20:01:17 +0900

    chore: 환경변수 설정 (#120)

--- set-version.sh
+++ set-version.sh
@@ -3,17 +3,21 @@
 ENV_FILE=".env"
 VERSION_KEY="VITE_VERSION"
 COMMIT_KEY="VITE_COMMIT_HASH"
+YEAR_KEY="VITE_YEAR"
 
 VERSION_VALUE=$(git describe --tags --abbrev=0)
 COMMIT_VALUE=$(git rev-parse --short HEAD)
+YEAR_VALUE=$(date +%Y)
 
 # 기존 키 제거 및 나머지 보존
 grep -v "^$VERSION_KEY=" "$ENV_FILE" 2>/dev/null | \
-grep -v "^$COMMIT_KEY=" > "$ENV_FILE.tmp" || touch "$ENV_FILE.tmp"
+grep -v "^$COMMIT_KEY=" | \
+grep -v "^$YEAR_KEY=" > "$ENV_FILE.tmp" || touch "$ENV_FILE.tmp"
 
 # 새 값 추가
 echo "$VERSION_KEY=$VERSION_VALUE" >> "$ENV_FILE.tmp"
 echo "$COMMIT_KEY=$COMMIT_VALUE" >> "$ENV_FILE.tmp"
+echo "$YEAR_KEY=$YEAR_VALUE" >> "$ENV_FILE.tmp"
 
 # 덮어쓰기
 mv "$ENV_FILE.tmp" "$ENV_FILE"
