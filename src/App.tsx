2d7e17528bb4f432499bb6f0ba211c500a6fa5c3
hyunmin0317 <hyunmin-choi@naver.com>
2025-10-10 09:55:51 +0900

    feat: Google Analytics 설정 (#119)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import {useEffect} from 'react'
+import {useLocation} from 'react-router-dom'
+import ReactGA from 'react-ga4'
+
 import MainRouter from './router/MainRouter'
 import './styles/App.css'
 
+const GA_ID = import.meta.env.VITE_GA_ID
+
+if (GA_ID) {
+  ReactGA.initialize(GA_ID, {
+    gtagOptions: {send_page_view: false}
+  })
+}
+
 function App() {
+  const location = useLocation()
+  useEffect(() => {
+    if (GA_ID && location.pathname) {
+      ReactGA.send({
+        hitType: 'pageview',
+        page: location.pathname
+      })
+    }
+  }, [location])
+
   return <MainRouter />
 }
 
